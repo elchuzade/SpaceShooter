8f7750c7e6c19ff68d71b7c671a124aa623c8534
Kamran Elchuzade <elchuzade@gmail.com>
2019-09-13 20:28:16 +0300

    created a spaceship with all weapons on it, made the spaceship follow the mouse

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] GameObject Spaceship;
+    [SerializeField] Image FrontShoot;
+    [SerializeField] Image SideShoot;
+    [SerializeField] Image Body;
+    [SerializeField] Image Rocket;
+
+    public string[] weapons;
+
+    public int score;
+    private bool rocket;
+    private bool frontShoot;
+    private bool sideShoot;
+
+    public void MoveSpaceship()
+    {
+        // Move object based on mouse coordinates
+        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+    }
+
+    private void Awake()
+    {
+        
+    }
+
+    private void Update()
+    {
+        MoveSpaceship();
+    }
+
+    // Hide weapons
+    public void FrontShootHide()
+    {
+        FrontShoot.enabled = false;
+    }
+    public void SideShootHide()
+    {
+        SideShoot.enabled = false;
+    }
+    public void RocketHide()
+    {
+        Rocket.enabled = false;
+    }
+    // Show weapons
+    public void FrontShootShow()
+    {
+        FrontShoot.enabled = true;
+    }
+    public void SideShootShow()
+    {
+        SideShoot.enabled = true;
+    }
+    public void RocketShow()
+    {
+        Rocket.enabled = true;
+    }
+}
