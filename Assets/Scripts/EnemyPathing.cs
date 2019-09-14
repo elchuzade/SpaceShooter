e1055707002e185fae472179bd2c6ae814b4d4a7
Kamran Elchuzade <elchuzade@gmail.com>
2019-09-14 13:10:02 +0300

    added waypoints to make the enemy follow the path

---
+++ Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    [SerializeField] WaveConfig waveConfig;
+    List<Transform> waypoints;
+    private int waypointIndex;
+    [SerializeField] float enemySpeed = 200f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waypoints = waveConfig.GetWaypoints();
+        Debug.Log(waypoints[waypointIndex].position.x + " - " + waypoints[waypointIndex].position.y);
+        transform.position = waypoints[waypointIndex].position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (waypointIndex < waypoints.Count)
+        {
+            var targetPosition = waypoints[waypointIndex].position;
+            var movementThisFrame = enemySpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/WaveConfig.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy Wave Config")]
+public class WaveConfig : ScriptableObject
+{
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] GameObject pathPrefab;
+    [SerializeField] float timeBetweenSpawns = 1f;
+    [SerializeField] float spawnRandomFactor = 0.5f;
+    [SerializeField] int numberOfEnemies = 10;
+    [SerializeField] float moveSpeed = 200f;
+
+    public GameObject GetEnemyPrefab() { return enemyPrefab; }
+    public float GetTimeBetweenSpawns() { return timeBetweenSpawns; }
+    public float GetSpawnRandomFactor() { return spawnRandomFactor; }
+    public int GetNumberOfEnemies() { return numberOfEnemies; }
+    public float GetMoveSpeed() { return moveSpeed; }
+
+    public List<Transform> GetWaypoints()
+    {
+        var waveWaypoints = new List<Transform>();
+        foreach(Transform child in pathPrefab.transform)
+        {
+            waveWaypoints.Add(child);
+        }
+        return waveWaypoints;
+    }
+}
